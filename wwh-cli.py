665bcc0456938d437e17c2171ae5147a1b7dea13
GI_Jack <GI_Jack@hackermail.com>
2024-05-05 19:12:28 -0700

    added some more boilerplate for argparse and versioning

--- wwh-cli.py
+++ wwh-cli.py
@@ -1,3 +1,36 @@
 #!/usr/bin/env python3
+app_desc='''wwh-cli.py - Wireplumper W31rd Hax
+Enable and Disable bundled config files that fix various problems or enact
+various effects.
+
+    USAGE:
+wwh-cli.py <command> [options]
+
+    COMMANDS:
+list    -   List available "hacks" are available, and which of those are enabled
+
+enable  -   Enable a hack, specify the name of the hack
+
+disable -   Disable a hack, specify the name of the hack
+'''
 
 from .wwh import *
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description=app_desc,epilog="\n\n",add_help=False,formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("command", nargs="?"   , help="See above for description of commands")
+    parser.add_argument("-?", "--help"         , help="Show This Help Message", action="help")
+    parser.add_argument("-v","--version"       , help="Print Version and Exit", action="store_true")
+    parser.add_argument("-t","--terse"         , help="output as CSV rather than a formatted table, useful for scripts", action="store_true")
+    args = parser.parse_args()
+    
+    if args.version == True:
+        message_line = "Wireplumber W13rd Hax VERSION: %s" % (_lib_meta.lib_ver)
+        print(message_line)
+        sys.exit(4)
+    
+
+if __name__ == "__main__":
+    main()
--- wwh/__init__.py
+++ wwh/__init__.py
@@ -4,3 +4,7 @@ wwh - Wireplumber W31rd Hax
 
 Common python library for GUI and CLI
 '''
+
+class _lib_meta:
+    lib_ver = "0.1.0"
+
