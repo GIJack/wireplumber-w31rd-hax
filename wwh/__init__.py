0563843db1427e0ca74a315cf8908c0312d2a5b0
GI_Jack <GI_Jack@hackermail.com>
2024-05-05 19:23:46 -0700

    added some default variables

--- wwh/__init__.py
+++ wwh/__init__.py
@@ -5,6 +5,11 @@ wwh - Wireplumber W31rd Hax
 Common python library for GUI and CLI
 '''
 
+import os
+
 class _lib_meta:
     lib_ver = "0.1.0"
 
+class default_var:
+    hax_dirs               = ['/usr/share/wireplumber-w31rd-hax','/usr/local/share/wireplumber-w31rd-hax']
+    wireplumber_config_dir = "%s/.config/wireplumber/wireplumber.conf.d/" % os.getenv("HOME")
