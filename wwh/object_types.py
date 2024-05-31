e08597ca4e133b810d218ddb06baf9509f45535a
GI_Jack <GI_Jack@hackermail.com>
2024-05-31 11:30:25 -0700

    add object type for config item

---
+++ wwh/object_types.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+config_item_len   = 4
+blank_config_line = [""] * config_item_len
+
+#filename, title, description, enabled
+
+class config_item:
+    def __init__(self,item_line=blank_config_line):
+        self.filename    = item_line[0]
+        self.title       = item_line[1]
+        self.description = item_line[2]
+        self.enabled     = item_line[3]
+    def __str__(self):
+        return(self.filename)
+    def __repr__(self):
+        out_line = "<wwd config item %s:%s>" % (self.title,self.filename)
+        return(out_line)
