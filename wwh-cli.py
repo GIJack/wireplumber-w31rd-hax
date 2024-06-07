1bc7987ab026eaf0d4adc70bc53f99b442efb551
GI_Jack <GI_Jack@hackermail.com>
2024-06-07 13:36:34 -0700

    added list command in cli, as well as arguments parameter on argparse

--- wwh-cli.py
+++ wwh-cli.py
@@ -18,9 +18,35 @@ from .wwh import *
 import argparse
 import sys
 
+# for terse
+delimeter = ","
+
+def cli_list_configs(terse=False):
+    '''Print a table with status. Show all hax and if they are enabled. if terse is True, print CSV instead of pretty formatting'''
+    tab_spaces = 15
+    available_configs = load_available_configs()
+    
+    # Print the Header Line, if not terse
+    if terse == False:
+        header_line = "Enabled\tName\tDescription"
+        header_line = header_line.expandtabs(tab_spaces)
+        print(header_line)
+        
+    for config in available_configs:
+        if terse == False:
+            out_line = "%s\t%s\t%s" % (config.enabled,config.title,config.description)
+            out_line = out_line.expandtables(tab_spaces)
+            print(out_line)
+        elif terse == True:
+            out_line = config.enabled + delimeter + config.title + delimeter + config.description
+            print(out_line)
+        else:
+            raise "terse is neither true nor false, should not be here. debug app"
+
 def main():
     parser = argparse.ArgumentParser(description=app_desc,epilog="\n\n",add_help=False,formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("command", nargs="?"   , help="See above for description of commands")
+    parser.add_argument("command",   nargs="?" , help="See above for description of commands")
+    parser.add_argument("arguments", nargs="*" , help="Arguments for command, see above")
     parser.add_argument("-?", "--help"         , help="Show This Help Message", action="help")
     parser.add_argument("-v","--version"       , help="Print Version and Exit", action="store_true")
     parser.add_argument("-t","--terse"         , help="output as CSV rather than a formatted table, useful for scripts", action="store_true")
@@ -31,6 +57,8 @@ def main():
         print(message_line)
         sys.exit(4)
     
+    if args.command == "list":
+        cli_list_configs()
 
 if __name__ == "__main__":
     main()
